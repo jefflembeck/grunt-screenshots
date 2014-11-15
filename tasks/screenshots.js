16d7b7dc1330ba9f94aed822b7da9eed111bca06
Jeffrey Lembeck <jlembeck@gmail.com>
2014-11-14 16:17:23 -0800

    Code

--- tasks/screenshots.js
+++ tasks/screenshots.js
@@ -6,44 +6,22 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
-
+/*global require:true*/
 module.exports = function(grunt) {
+  'use strict';
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+  var screenshot = require( "../lib/shot-wrapper" );
 
-  grunt.registerMultiTask('screenshots', 'Use Grunt and PhantomJS to generate Screenshots of pages', function() {
+  grunt.registerMultiTask( 'screenshots', 'Use Grunt and PhantomJS to generate Screenshots of pages', function() {
+    var done = this.async();
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      punctuation: '.',
-      separator: ', '
+      url: '',
+      output: ''
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+    screenshot.takeShot( options.url, options.output, function(){
+      done();
     });
   });
 
